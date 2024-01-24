8dbb27ba2b0842e65f3314c886eb4270d58e7d7f
Axhyut <137641003+Axhyut@users.noreply.github.com>
2024-01-24 20:01:51 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+            "A. Cascading Style Sheet",
+            "B. Cascading Stylish Sheet",
+            "C. Computer Style Sheet",
+            "D. Computer Styler Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "A. Hypertext Preprocessor",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. Hometext Programming",
+            "C. Hypertext Preprogramming",
+            "D. Pre Hypertext Programming"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "D. Structured Query Language",
+        options: [
+            "A. Sorted Query Language",
+            "B. Stylesheet Query Langauge",
+            "C. Structured Question Language",
+            "D. Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "B. Extensible Markup Language",
+        options: [
+            "A. Extension Multi Language",
+            "B. Extensible Markup Language",
+            "C. Extra Markup Language",
+            "D. Extensible Multiple Language"
+        ]
+    }
+]
